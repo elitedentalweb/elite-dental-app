b179b1b5557410c5b3b50d59c5abaf1310e1f58a
RenatMoroz <renatmoroz.dev@gmail.com>
2026-06-19 01:48:45 +0300

    add route reset-forgot

---
+++ app/api/auth/reset-password/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { globalApi } from '../../serverConfig';
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json();
+    const result = await globalApi.post('/auth/reset-password', body);
+    return NextResponse.json(result.data);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return NextResponse.json(
+      error?.response?.data || { message: 'Reset password failed' },
+      { status: error?.response?.status || 500 }
+    );
+  }
+};
